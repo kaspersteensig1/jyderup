6ecab4b7560ec2aeedaa817cd6eb9c63a5d95179
Kasper Steensig <steensigk@gmail.com>
2018-07-09 17:51:18 +0200

    test menu mobil

--- script/functions.js
+++ script/functions.js
@@ -1,17 +1,27 @@
-$( document ).ready(function() {
-    
-    
-    $('#categoriMenu > a').click(function() {
+$(document).ready(function () {
+
+
+    $('#categoriMenu > a').click(function () {
         $(this).children().toggleClass('fa-angle-up')
         $(this).children().toggleClass("fa-angle-down");
-});
-    
-    
-    
-    $('#filterButton').click(function() {
+    });
+
+
+
+    $('#filterButton').click(function () {
         $('#filter').toggleClass('showContent');
     });
-    
+
+    //move aside menu..
+
+
+    $(window).resize(function () {
+        if ($(window).width() <= 991) {
+            $('#filter').prependTo('body');
+        }
+        else {
+            $('#filter').appendTo('aside');
+        }
+    });
+
 });
-
-
